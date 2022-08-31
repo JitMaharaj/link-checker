f3280f8bea10248886d7bcc71362d1d869520841
Roberto Tassone <roberto.tassone@proton.ch>
2022-08-31 18:25:34 +0200

    add getLinkStatus method

--- src/LinkChecker.php
+++ src/LinkChecker.php
@@ -5,10 +5,38 @@ namespace LinkChecker;
 class LinkChecker
 {
     /**
+     * @param string $link              link to check
+     * @param bool $verifyCertificate   if false it will not verify the SSL certificate
+     * @param bool $verbose             if true it will print cURL details
+     * @return string                   link status: online/offline/invalid
+     * @throws Exception                connection failed
+     */
+    public static function getLinkStatus(
+        string $link,
+        bool $verifyCertificate = true,
+        bool $verbose = false,
+    ): string {
+        $link = trim($link);
+        $status = Constants::STATUS_INVALID;
+        /** @var CheckerInterface $host */
+        foreach ([Mega::class, GoogleDrive::class] as $host) {
+            if ($host::isValid($link)) {
+                $status = $host::isOnline(
+                    $link,
+                    $verifyCertificate,
+                    $verbose
+                ) ? Constants::STATUS_ONLINE : Constants::STATUS_OFFLINE;
+                break;
+            }
+        }
+        return $status;
+    }
+    /**
      * @param string[] $links           array of links
      * @param bool $continueOnError     if true the script will not be interrupted if a link in invalid
      * @param bool $verifyCertificate   if false it will not verify the SSL certificate
-     * @return string[]                 array with the structure ['link' => 'online/offline/invalid']
+     * @param bool $verbose             if true it will print cURL details
+     * @return string[]                 array with the structure ['link' => 'online/offline/invalid/unknown']
      */
     public static function checkLinks(
         array $links,
@@ -18,26 +46,14 @@ class LinkChecker
     ): array {
         $result = [];
         foreach ($links as $link) {
-            $link = trim($link);
-            $status = Constants::STATUS_INVALID;
-            /** @var CheckerInterface $host */
-            foreach ([Mega::class, GoogleDrive::class] as $host) {
-                try {
-                    if ($host::isValid($link)) {
-                        $status = $host::isOnline(
-                            $link,
-                            $verifyCertificate,
-                            $verbose
-                        ) ? Constants::STATUS_ONLINE : Constants::STATUS_OFFLINE;
-                        break;
-                    }
-                } catch (\Exception $e) {
-                    if ($continueOnError) {
-                        $status = Constants::STATUS_UNKNOWN;
-                        break;
-                    } else {
-                        throw $e;
-                    }
+            try {
+                $status = self::getLinkStatus($link, $verifyCertificate, $verbose);
+            } catch (\Exception $e) {
+                if ($continueOnError) {
+                    $status = Constants::STATUS_UNKNOWN;
+                    break;
+                } else {
+                    throw $e;
                 }
             }
             $result[$link] = $status;
--- tests/LinkCheckerTest.php
+++ tests/LinkCheckerTest.php
@@ -4,11 +4,34 @@ declare(strict_types=1);
 
 use LinkChecker\Constants;
 use LinkChecker\LinkChecker;
-use LinkChecker\Mega;
 use PHPUnit\Framework\TestCase;
 
 final class LinkCheckerTest extends TestCase
 {
+
+    public function testGetLinkStatus(): void
+    {
+        $linksToCheck = [
+            'https://mega.nz/file/xxxxxxxx#zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz' => Constants::STATUS_OFFLINE,
+            'https://mega.nz/fil/xxxxxxxx#zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz' => Constants::STATUS_INVALID,
+            'https://mega.nz/file/xxxxxxxx#' => Constants::STATUS_OFFLINE,
+            'https://mega.nzfile/xxxxxxxx#' => Constants::STATUS_INVALID,
+            'https://mega.nz/#!xxxxxxxx!zzzzzzzzzzzzzzzzzzzzz' => Constants::STATUS_OFFLINE,
+            'https://mega.nz/#!xxxxxxxxzzzzzzzzzzzzzzzzzzzzz' => Constants::STATUS_INVALID,
+            'https://drive.google.com/file/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/view' => Constants::STATUS_OFFLINE,
+            'https://drive.google.com/file/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx/error' => Constants::STATUS_INVALID,
+            'https://drive.google.com/file/d/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxtoolong' => Constants::STATUS_INVALID,
+            'https://drive.google.com/file/d/xxxxxxxxxxxxxxxxxtooshort' => Constants::STATUS_INVALID,
+            'https://drive.google.com/drive/folders/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' => Constants::STATUS_OFFLINE,
+            'https://drive.google.com/drive/folders/xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxtoolong' => Constants::STATUS_INVALID,
+            'https://drive.google.com/drive/folders/xxxxxxxxxxxxxxxxtooshort' => Constants::STATUS_INVALID,
+        ];
+
+        foreach ($linksToCheck as $link => $expectedStatus) {
+            $this->assertSame(LinkChecker::getLinkStatus($link), $expectedStatus, $link);
+        }
+    }
+
     public function testCheckLinks(): void
     {
         $linksToCheck = [
